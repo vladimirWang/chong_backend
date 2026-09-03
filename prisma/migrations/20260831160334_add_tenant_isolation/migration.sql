3bbf112b94ca8e860182ebc6d876471749d6b4a6
fernando <413114463@qq.com>
2026-09-03 13:18:17 +0800

    修正sql迁移文件

--- prisma/migrations/20260831160334_add_tenant_isolation/migration.sql
+++ prisma/migrations/20260831160334_add_tenant_isolation/migration.sql
@@ -18,6 +18,7 @@ CREATE TABLE `Tenant` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     UNIQUE INDEX `Tenant_code_key`(`code`),
+    UNIQUE INDEX `Tenant_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
