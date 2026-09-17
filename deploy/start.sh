3ac232e506abf4fc99dcb985fc2899443d132bd6
fernando <413114463@qq.com>
2026-09-17 13:43:43 +0800

    build时不构建Image-service

--- deploy/start.sh
+++ deploy/start.sh
@@ -9,7 +9,7 @@ COMPOSE="docker compose -p repo-prod --env-file ../.env.prod"
 # 串行构建（而非 up --build 的并行构建），削平内存/磁盘峰值，
 # 避免小内存机器上 BuildKit 同时构建多个镜像导致 OOM/swap 颠簸整机夯死
 $COMPOSE build server
-$COMPOSE build image-service
+# $COMPOSE build image-service
 $COMPOSE build nginx
 
 $COMPOSE up -d
